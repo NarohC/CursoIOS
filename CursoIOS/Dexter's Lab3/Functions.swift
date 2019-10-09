08dccb6cf98f67e9c99a503d15baa6a58bdd5531
NarohC <narohcramhen@gmail.com>
2019-10-09 08:53:01 -0600

    Funciones dentro de funciones

--- CursoIOS/Dexter's Lab3/Functions.swift
+++ CursoIOS/Dexter's Lab3/Functions.swift
@@ -111,3 +111,15 @@ var v1 = 3
 var v2 = 107
 swapTwoInts(&v1, &v2)
 print("v1 ahora es \(v1) y v2 ahora es \(v2)")
+
+let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+
+func backward(_ s1: String, _ s2: String) -> Bool {
+    return s1 > s2
+}
+
+var reversedNames = names.sorted(by: backward)
+
+var reversedNames = names.sorted(by: { (s1:String ,  s2:String) -> Bool in return s1 > s2})
+
+var reversedNames = names.sorted(by: {$0 > $1})
